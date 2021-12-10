5018d58e008bfab4543cdf092d6dad8f9b893277
Artem Neretin <hawktrike@gmail.com>
2021-12-10 19:53:22 +0700

    test(plasma-b2c,plasma-web,plasma-ui): Add test and update snapshots for `Tab` component

--- packages/plasma-core/src/components/Tabs/Tabs.component-test.tsx
+++ packages/plasma-core/src/components/Tabs/Tabs.component-test.tsx
@@ -18,4 +18,40 @@ describe('plasma-core: Tabs', () => {
 
         cy.matchImageSnapshot();
     });
+
+    it('scrollable', () => {
+        const Container = ({ children }) => {
+            return <div style={{ width: '75px' }}>{children}</div>;
+        };
+
+        mount(
+            <CypressTestDecorator>
+                <Container>
+                    <Tabs>
+                        <TabItem>Joy</TabItem>
+                        <TabItem isActive>Sber</TabItem>
+                        <TabItem>Eva</TabItem>
+                    </Tabs>
+                </Container>
+            </CypressTestDecorator>,
+        );
+
+        cy.get('[role="tablist"]').parent().scrollTo(500, 0);
+
+        cy.matchImageSnapshot();
+    });
+
+    it('_stretch', () => {
+        mount(
+            <CypressTestDecorator>
+                <Tabs stretch>
+                    <TabItem>Joy</TabItem>
+                    <TabItem isActive>Sber</TabItem>
+                    <TabItem>Eva</TabItem>
+                </Tabs>
+            </CypressTestDecorator>,
+        );
+
+        cy.matchImageSnapshot();
+    });
 });
